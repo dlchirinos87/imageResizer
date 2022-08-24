44ac49969a57a806f272709c20931d870791c17d
danic <jagfootwear@Jags-MacBook-Pro.local>
2022-08-23 20:09:08 -0400

    first commit

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import cv2
 import glob
 
 img_list = glob.glob("originalImages/*")
-img_list.reverse()
+
 
 a = 1
-for img in img_list:
+for img in reversed(img_list):
     image = cv2.imread(img, 1)
     width = 1000
     height = int(image.shape[0]*1000/image.shape[1])
